52c8d11fe33052fae1ad68c24dde0324143c0341
Regis Ranniere <regis.ranniere@outlook.com>
2020-05-27 16:39:54 -0300

    Autenticação inicial de usuário.

---
+++ src/app/Controllers/Http/TokenController.js
@@ -0,0 +1,17 @@
+'use strict'
+
+class TokenController {
+    async store({ request, auth }){
+        const { email, password } = request.only([
+            'email',
+            'password'
+        ]);
+
+        const token = await auth.attempt(email, password);
+
+        return { token };
+
+    }
+}
+
+module.exports = TokenController
